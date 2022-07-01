caa081c4b9fa0ec2021f378b67add17a35f4c813
Malik Johnson <60333731+MalikJohnson@users.noreply.github.com>
2022-06-30 20:45:01 -0400

    Added data structures to mirror C#
    Added data structures to mimic the C# Linq vs Streams syntax test

--- src/StreamVsLinq.java
+++ src/StreamVsLinq.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class StreamVsLinq {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Using same data structures as the linq in C# to test syntax
+		
+				List<String> words = new ArrayList<String>();
 
+				HashMap<String,String> hashMap =   new HashMap<String,String>() ;
+
+				int[] testNum = { 50, 1, -1, 66, -80, 1000, 682, -972, 0, 30, 20, 66, 94, 28 };
+				
+				
 	}
 
 }
